e9bda7605421a22db9d4e0939ff2af759f7be13b
carlsoerensen <102477743+carlsoerensen@users.noreply.github.com>
2023-09-08 10:22:35 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,22 @@
-fetch("https://hgxphlvxhzinnokdclkh.supabase.co/rest/v1/Data?apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhneHBobHZ4aHppbm5va2RjbGtoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM5OTAyMDcsImV4cCI6MjAwOTU2NjIwN30.rxRj6VAFxUXkCNfe8DMj0SidSXcKpBTLtSL9CAElxMU")
-.then(res=>res.json())
-.then(showHeroes)
-
+fetch(
+  "https://hgxphlvxhzinnokdclkh.supabase.co/rest/v1/Data?apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhneHBobHZ4aHppbm5va2RjbGtoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM5OTAyMDcsImV4cCI6MjAwOTU2NjIwN30.rxRj6VAFxUXkCNfe8DMj0SidSXcKpBTLtSL9CAElxMU"
+)
+  .then((res) => res.json())
+  .then(showSvampe);
 
 function showSvampe(svampe) {
-svampe.forEach(showSvamp);
+  svampe.forEach(showSvamp);
 }
 
 function showSvamp(svamp) {
-const template = document.querySelector("template").content;
-const copy = template.cloneNode(true);
+  const template = document.querySelector("template").content;
+  const copy = template.cloneNode(true);
 
-copy.querySelector("h2").textContent = svamp.title;
-copy.querySelector("p span.isEvil").textContent = hero.isEvil;
-copy.querySelector("p.powers").textContent = hero.powers;
-copy.querySelector("p.weaknesses").textContent = hero.weaknesses;
-copy.querySelector("img").src = svamp.profile_image/src;
-const parent = document.querySelector(".grid");
-parent.appendChild(copy);
+  copy.querySelector("h2").textContent = svamp.id;
+  copy.querySelector(".title_").textContent = svamp.title;
+  copy.querySelector(".kategori_").textContent = svamp.categories_name;
+  //copy.querySelector(".season_0").textContent = svamp.season_0;
+  copy.querySelector("img").src = svamp.profile_image_src;
+  const parent = document.querySelector(".grid");
+  parent.appendChild(copy);
 }
