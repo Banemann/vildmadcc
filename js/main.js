f97f7b59ea8c6ef92b480000cdfa6dcd222379eb
carlsoerensen <102477743+carlsoerensen@users.noreply.github.com>
2023-09-13 11:35:18 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,50 @@
+// Define a variable to store the selected filter option
+let selectedFilter = "all";
+
+// Get the filter form and the filteredResults div
+const filterForm = document.getElementById("filterForm");
+const filteredResults = document.getElementById("filteredResults");
+
+// Add an event listener to the form to handle filtering
+filterForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  selectedFilter = document.getElementById("mushroomType").value;
+  filterSvampe();
+});
+
+// Fetch and filter the mushrooms initially
 fetch(
   "https://hgxphlvxhzinnokdclkh.supabase.co/rest/v1/Data?apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhneHBobHZ4aHppbm5va2RjbGtoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM5OTAyMDcsImV4cCI6MjAwOTU2NjIwN30.rxRj6VAFxUXkCNfe8DMj0SidSXcKpBTLtSL9CAElxMU"
 )
   .then((res) => res.json())
-  .then(showSvampe);
+  .then((data) => {
+    if (Array.isArray(data)) {
+      showSvampe(data);
+      filterSvampe();
+    } else {
+      console.error("Data is not an array:", data);
+    }
+  })
+  .catch((error) => {
+    console.error("Error fetching data:", error);
+  });
+
+// Function to filter and display mushrooms based on the selected filter
+function filterSvampe() {
+  const mushrooms = document.querySelectorAll(".svamp");
+  mushrooms.forEach((svamp) => {
+    const isEatable = svamp.dataset.isEatable === "true";
+    if (
+      selectedFilter === "all" ||
+      (selectedFilter === "eatable" && isEatable) ||
+      (selectedFilter === "noneatable" && !isEatable)
+    ) {
+      svamp.style.display = "block";
+    } else {
+      svamp.style.display = "none";
+    }
+  });
+}
 
 function showSvampe(svampe) {
   svampe.forEach(showSvamp);
@@ -19,18 +61,15 @@ function showSvamp(svamp) {
     ".description"
   ).textContent = `Kan findes i ${svamp.sankelandskaber_title}`;
 
-  copy.querySelector(".spiselig").textContent = `Spiselig: ${svamp.isEatable}`;
+  copy.querySelector(".spiselig").textContent = svamp.isEatable
+    ? "Svampen er spiselig"
+    : "Svampen er ikke spiselig, hold øje!";
 
-  if (svamp.isEatable === true) {
-    copy.querySelector(".spiselig").textContent = `Svampen er spiselig`;
-  } else {
-    copy.querySelector(
-      ".spiselig"
-    ).textContent = `Svampen er ikke spiselig, hold øje!`;
-  }
-
-  //copy.querySelector(".season_0").textContent = svamp.season_0;
   copy.querySelector("img").src = svamp.profile_image_src;
+
+  // Store the isEatable value as a data attribute for filtering
+  copy.querySelector(".svamp").dataset.isEatable = svamp.isEatable;
+
   const parent = document.querySelector(".grid");
   parent.appendChild(copy);
 }
